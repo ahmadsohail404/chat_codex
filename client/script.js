4f0d7e67e393122a290649033a355b70fb8092f9
Sohail Ahmad <sohailss2412@gmail.com>
2023-03-20 00:08:55 +0530

    backend deployed

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId)
     loader(messageDiv)
 
-    const response = await fetch('https://chat-codex-o3es.onrender.com', {
+    const response = await fetch('https://wandering-hose-ox.cyclic.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
